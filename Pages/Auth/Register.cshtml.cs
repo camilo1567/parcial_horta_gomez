ae5d07b7bfcbf50b22496abc1cddc228e9867ddb
camilo1567 <camilo20113@hotmail.com>
2023-05-22 23:10:05 -0500

    primer commit ajuste de tailwind

---
+++ Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace parcial_Horta_Gomez.Pages.Auth
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
