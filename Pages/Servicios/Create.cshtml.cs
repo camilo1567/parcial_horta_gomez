520a33816843b81c8a77d90434855cc16d03d82b
camilo1567 <camilo20113@hotmail.com>
2023-05-24 18:51:52 -0500

    creacion de structura de vistas

---
+++ Pages/Servicios/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace parcial_Horta_Gomez.Pages.Servicios
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Servicios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace parcial_Horta_Gomez.Pages.Servicios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
