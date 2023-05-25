3ac025970e3400bfce2d3cbd5fd4fcc797dc0ab1
Miguel Angel Martinez Gómez <auxti3@alsea.com.co>
2023-05-25 16:42:18 -0500

    Delete Facturas

--- Pages/Facturas/Delete.cshtml.cs
+++ Pages/Facturas/Delete.cshtml.cs
@@ -1,12 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Facturas
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public DeleteModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Factura Factura { get; set; } = default!;
+
+        public async Task<IActionResult> OngetAsync(int? id)
+        {
+            if (id == null || _context.Facturas == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (factura == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Factura = factura;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Facturas == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas.FindAsync(id);
+
+            if (factura != null)
+            {
+                Factura = factura;
+                _context.Facturas.Remove(Factura);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+
         }
     }
 }
