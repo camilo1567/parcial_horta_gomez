fa5e10937994fbae5d56a184893568996cbe726c
Miguel Angel Martinez Gómez <auxti3@alsea.com.co>
2023-05-25 12:01:06 -0500

    Delete Clientes

--- Pages/Clientes/Edit.cshtml.cs
+++ Pages/Clientes/Edit.cshtml.cs
@@ -1,12 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Clientes
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public EditModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Cliente Cliente { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            Cliente = cliente;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!ClienteExists(Cliente.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
