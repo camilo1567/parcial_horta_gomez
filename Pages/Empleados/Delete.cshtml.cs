1964e2fce214ecf0fe09f667f3944143bc120ef0
camilo1567 <camilo20113@hotmail.com>
2023-05-24 19:10:18 -0500

    estructura vistas de empleados

---
+++ Pages/Empleados/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace parcial_Horta_Gomez.Pages.Empleados
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
