a6c41ea6cfe85cc5220472a3cd10b16eea6aa55a
Miguel Angel Martinez Gómez <auxti3@alsea.com.co>
2023-05-25 14:43:49 -0500

    Index-Create Empleados

--- Pages/Empleados/Create.cshtml.cs
+++ Pages/Empleados/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Empleados
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public CreateModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+
+        public Empleado Empleado { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Empleados == null || Empleado == null)
+            {
+                return Page();
+            }
+
+            _context.Empleados.Add(Empleado);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
--- Pages/Empleados/Index.cshtml.cs
+++ Pages/Empleados/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Empleados
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public IndexModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        public IList<Empleado> Empleados { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Empleados != null)
+            {
+                Empleados = await _context.Empleados.ToListAsync();
+            }
         }
     }
 }
