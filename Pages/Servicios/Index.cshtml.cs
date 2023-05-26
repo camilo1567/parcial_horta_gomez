202aef48e15a4012afc0fa5742d7fe61d87c810b
camilo1567 <camilo20113@hotmail.com>
2023-05-25 22:00:17 -0500

    ajuste en vistas y continuacion crud servicios

--- Pages/Servicios/Index.cshtml.cs
+++ Pages/Servicios/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Servicios
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public IndexModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        public IList<Servicio> Servicios { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Servicios != null)
+            {
+                Servicios = await _context.Servicios.ToListAsync();
+            }
         }
     }
 }
