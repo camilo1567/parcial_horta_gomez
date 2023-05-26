075d20bcde6596e7ad08eadb24d193fbed24636f
camilo1567 <camilo20113@hotmail.com>
2023-05-25 22:18:07 -0500

    editar y eliminar servicios

--- Pages/Servicios/Delete.cshtml.cs
+++ Pages/Servicios/Delete.cshtml.cs
@@ -1,12 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Servicios
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public DeleteModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Servicio Servicio { get; set; } = default!;
+
+        public async Task<IActionResult> OngetAsync(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Servicio = servicio;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FindAsync(id);
+
+            if (servicio != null)
+            {
+                Servicio = servicio;
+                _context.Servicios.Remove(Servicio);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+
         }
     }
 }
--- Pages/Servicios/Edit.cshtml.cs
+++ Pages/Servicios/Edit.cshtml.cs
@@ -1,12 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using parcial_Horta_Gomez.Data;
+using parcial_Horta_Gomez.Models;
 
 namespace parcial_Horta_Gomez.Pages.Servicios
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly RostrosFelicesContext _context;
+
+        public EditModel(RostrosFelicesContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Servicio Servicio { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            Servicio = servicio;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Servicio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!ServicioExists(Servicio.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ServicioExists(int id)
+        {
+            return (_context.Servicios?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
